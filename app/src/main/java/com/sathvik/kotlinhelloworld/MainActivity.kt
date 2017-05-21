fac7042159c317e933fdff0a9ad742319fcfb7ec
Sathvik Parekodi <sathvik@lendstar.io>
2017-05-21 23:50:21 +0200

    Kotlin Button Click example

--- app/src/main/java/com/sathvik/kotlinhelloworld/MainActivity.kt
+++ app/src/main/java/com/sathvik/kotlinhelloworld/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.sathvik.kotlinhelloworld
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,11 +15,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
-
-        val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "First project in Kotlin", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        
+        val clickMeBtn = findViewById(R.id.click_me) as Button
+        clickMeBtn.setOnClickListener { view ->
+            Toast.makeText(this,"You clicked me !! ", Toast.LENGTH_SHORT).show()
         }
     }
 
